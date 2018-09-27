dbf07eeed4ed5cd24cea290f239561725cd88fd8
NJ <office@webproject.waw.pl>
2018-09-27 20:20:44 +0200

    switch from exact match to ratio hotword match for better results, switch to tabs

--- main.py
+++ main.py
@@ -36,140 +36,158 @@ import requests
 import json
 import threading
 
+from difflib import SequenceMatcher
+
 
 logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+	level=logging.INFO,
+	format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
 )
 
 _remoteUrl = 'http://8.8.8.8/home/executables/';
 _items = []
 
 def power_off_pi():
-    aiy.audio.say('Good bye!')
-    subprocess.call('sudo shutdown now', shell=True)
+	aiy.audio.say('Good bye!')
+	subprocess.call('sudo shutdown now', shell=True)
 
 
 def reboot_pi():
-    aiy.audio.say('See you in a bit!')
-    subprocess.call('sudo reboot', shell=True)
-    
+	aiy.audio.say('See you in a bit!')
+	subprocess.call('sudo reboot', shell=True)
+	
 def terminate():
-    aiy.audio.say('See you')
-    exit(-1)
+	aiy.audio.say('See you')
+	exit(-1)
 
 def runItem(itemId, status):
-    #aiy.audio.say('ok')
-    print('runItem ' + itemId + ' with status ' + status)
-    url = _remoteUrl + 'toggle.php';
-    postData = {'outletId' : itemId, 'outletStatus'  : status, 'outletDelayed': '0'}
-    headers = {'content-type': 'application/json'}
-    r = requests.post(url, data=json.dumps(postData), headers=headers)
-    
+	#aiy.audio.say('ok')
+	print('runItem ' + itemId + ' with status ' + status)
+	url = _remoteUrl + 'toggle.php';
+	postData = {'outletId' : itemId, 'outletStatus'  : status, 'outletDelayed': '0'}
+	headers = {'content-type': 'application/json'}
+	r = requests.post(url, data=json.dumps(postData), headers=headers)
+	
 def getDevicesData():
-    try:
-        global _items
-        url = _remoteUrl + 'Services.php';
-        postData = {'service':'CheckItemsData', 'receive' : '1', 'category'  : 'general'}
-        headers = {'content-type': 'application/json'}
-        r = requests.post(url, data=json.dumps(postData), headers=headers)
-        
-        #print(r.text)
-        items = json.loads(r.text)
-        if 'items' not in items:
-            return
-        
-        for item in items['items']:
-            if 'hotword' in item and item['hotword'] is not None:
-                print('Found configured device ' + item['id'] + ' by hotword ' + item['hotword'])
-                _items.append(item)
-        
-    except Exception as e:
-        print('Error during getDevicesData')
-        print(str(e))
-        pass
+	try:
+		global _items
+		url = _remoteUrl + 'Services.php';
+		postData = {'service':'CheckItemsData', 'receive' : '1', 'category'  : 'general'}
+		headers = {'content-type': 'application/json'}
+		r = requests.post(url, data=json.dumps(postData), headers=headers)
+		
+		#print(r.text)
+		items = json.loads(r.text)
+		if 'items' not in items:
+			return
+		
+		for item in items['items']:
+			if 'hotword' in item and item['hotword'] is not None:
+				print('Found configured device ' + item['id'] + ' by hotword ' + item['hotword'])
+				_items.append(item)
+		#print(_items)
+		
+	except Exception as e:
+		print('Error during getDevicesData')
+		print(str(e))
+		pass
+
+def similar(a, b):
+	return SequenceMatcher(None, a, b).ratio()
 
 def checkItem(name):
-    
-    status = None
-    if name.endswith(' off'):
-        status = 'off'
-        name = name[:-4]
-    elif name.endswith(' on'):
-        status = 'on'
-        name = name[:-3]
-    else:
-        return None
-    
-    for item in _items:
-        if item['hotword'] == name:
-            print('Found configured device ' + name)
-            return {'name':item['id'], 'status':status}
-    return None
+
+	status = None
+	if name.endswith(' off'):
+		status = 'off'
+		name = name[:-4]
+	elif name.endswith(' on'):
+		status = 'on'
+		name = name[:-3]
+	else:
+		return None
+
+	maxRatio = 0.0
+	maxItem = None
+
+	for item in _items:
+		currentRatio = similar(item['hotword'], name)
+		print('For item "' + item['id'] + '" with hotword "' + item['hotword'] + '" name of "' + name + '" gives score ' + str(currentRatio))
+		if currentRatio > maxRatio:
+				maxRatio = currentRatio
+				maxItem = item
+	
+	if maxItem is not None:
+		print('Found winner device "' + maxItem['id'] + '" with max score ' + str(maxRatio))
+		return {'name':maxItem['id'], 'status':status}
+
+	return None
 
 def say_ip():
-    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
-    aiy.audio.say('My IP address is %s' % ip_address.decode('utf-8'))
+	ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
+	aiy.audio.say('My IP address is %s' % ip_address.decode('utf-8'))
 
 
 def process_event(assistant, event):
-    print(event)
-    status_ui = aiy.voicehat.get_status_ui()
-    if event.type == EventType.ON_START_FINISHED:
-        status_ui.status('ready')
-        if sys.stdout.isatty():
-            print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+	print(event)
+	status_ui = aiy.voicehat.get_status_ui()
+	if event.type == EventType.ON_START_FINISHED:
+		status_ui.status('ready')
+		if sys.stdout.isatty():
+			print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
 
-    elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-        #status_ui.set_trigger_sound_wave('/home/pi/AIY_stuff/yup_sil.wav')
-        status_ui.status('listening')
+	elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
+		status_ui.set_trigger_sound_wave('/home/pi/AIY_stuff/yup_sil.wav')
+		status_ui.status('listening')
 
-    elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
-        print('You said:', event.args['text'])
-        text = event.args['text'].lower()
-        if text == 'power off':
-            assistant.stop_conversation()
-            power_off_pi()
-        elif text == 'reboot':
-            assistant.stop_conversation()
-            reboot_pi()
-        elif text == 'ip address':
-            assistant.stop_conversation()
-            say_ip()
-        elif text == 'terminate':
-            assistant.stop_conversation()
-            terminate()
-        else:
-            checked = checkItem(text)
-            if checked is not None:
-                t = threading.Thread(target=runItem, args=(checked['name'], checked['status']))
-                t.start()
-                assistant.stop_conversation()
+	elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
+		print('You said:', event.args['text'])
+		text = event.args['text'].lower()
+		if text == 'power off':
+			assistant.stop_conversation()
+			power_off_pi()
+		elif text == 'reboot':
+			assistant.stop_conversation()
+			reboot_pi()
+		elif text == 'ip address':
+			assistant.stop_conversation()
+			say_ip()
+		elif text == 'terminate':
+			assistant.stop_conversation()
+			terminate()
+		else:
+			checked = checkItem(text)
+			if checked is not None:
+				t = threading.Thread(target=runItem, args=(checked['name'], checked['status']))
+				t.start()
+				assistant.stop_conversation()
 
-    elif event.type == EventType.ON_END_OF_UTTERANCE:
-        status_ui.status('thinking')
+	elif event.type == EventType.ON_END_OF_UTTERANCE:
+		status_ui.status('thinking')
 
-    elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
-          or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
-          or event.type == EventType.ON_NO_RESPONSE):
-        status_ui.status('ready')
+	elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
+		  or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
+		  or event.type == EventType.ON_NO_RESPONSE):
+		status_ui.status('ready')
 
-    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
-        sys.exit(1)
+	elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+		sys.exit(1)
 
 
 def main():
-    if platform.machine() == 'armv6l':
-        print('Cannot run hotword demo on Pi Zero!')
-        exit(-1)
+	if platform.machine() == 'armv6l':
+		print('Cannot run hotword demo on Pi Zero!')
+		exit(-1)
 
-    getDevicesData()
+	getDevicesData()
+	
+	#exit(-1)
 
-    credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
-    with Assistant(credentials) as assistant:
-        for event in assistant.start():
-            process_event(assistant, event)
+	credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
+	with Assistant(credentials) as assistant:
+		for event in assistant.start():
+			process_event(assistant, event)
 
 
 if __name__ == '__main__':
-    main()
+	main()
